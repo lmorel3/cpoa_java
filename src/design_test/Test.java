78f08b3cbf4b93be519d98003dac9ae90134c4cd
Laurent MOREL <laurent.morel30@gmail.com>
2016-01-05 16:23:10 +0100

    AppController + model Manager + view Connection

--- src/design_test/Test.java
+++ src/design_test/Test.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 import model.Umpire;
 import model.UmpireCollection;
 import model.Connector;
+<<<<<<< Updated upstream
 import model.Person;
+=======
+import model.Manager;
+>>>>>>> Stashed changes
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,11 +24,29 @@ public class Test {
     
     public static void main(String args[]) {
         
+<<<<<<< Updated upstream
         Umpire maurice = UmpireCollection.read(4).get(0);
+=======
+        /*ArrayList<Object> params;
+        params = new ArrayList<>();
+        
+        params.add(4);
+        Connector co = Connector.getConnection();
+>>>>>>> Stashed changes
         
         maurice.setCountry("France");
         
+<<<<<<< Updated upstream
         UmpireCollection.update(maurice);
+=======
+        Umpire carver = new Umpire();
+        System.out.println(carver.getForename());
+        
+        System.out.println(UmpireCollection.read(4).get(0).getCountry());*/
+        
+        System.out.println(Manager.connectManager("toto", "tata"));
+
+>>>>>>> Stashed changes
         
     }
     
